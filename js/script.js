8ed02139fe0efded78e6a9ed3425a1a4fc6984b6
Yura Solomko <solomkoyv@gmail.com>
2018-10-24 21:24:32 +0300

    Yoga + Tabs + Timer + Smooth scroll2 + Modal + Delegir + ES6 + JSON + Promis + animForm + Slider + Calc

--- js/script.js
+++ js/script.js
@@ -121,6 +121,20 @@ window.addEventListener("DOMContentLoaded", function () {
                 behavior: "smooth"
             });
         }
+
+        // Slider
+        if (target && target.classList.contains('prev')) {
+            pluseSlides(-1);
+        }
+        if (target && target.classList.contains('next')) {
+            pluseSlides(1);
+        }
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (target.classList.contains('dot') && target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+
     });
 
     //  Form
@@ -186,12 +200,103 @@ window.addEventListener("DOMContentLoaded", function () {
     sendForm(form);
     sendForm(contactForm);
 
-    const inputsPhone = document.querySelectorAll('input[name="phone"]');
+    // Validation number phone
 
+    const inputsPhone = document.querySelectorAll('input[name="phone"]'),
+        inputsCounter = document.querySelectorAll('.counter-block-input');
+
+    // function onlyNumber(input) {
+    //     input.onkeydown = function () {
+    //         return (this.value = this.value.replace(/[^0-9]/g, ""));
+    //     };
+    // }
     function onlyNumber(input) {
         input.onkeydown = function () {
             return (this.value = this.value.replace(/[^0-9]/g, ""));
         };
     }
     [...inputsPhone].forEach(elem => onlyNumber(elem));
+    [...inputsCounter].forEach(elem => onlyNumber(elem));
+
+    // Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        // slideIndex = n > slides.length ? 1 : n;
+        // slideIndex = n < 1 ? slides.length : n;
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function pluseSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    // Calc 
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.textContent = 0;
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+    restDays.addEventListener('change', function () {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    place.addEventListener('change', function () {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            let a = total;
+            totalValue.textContent = a * this.options[this.selectedIndex].value;
+        }
+    });
+
 });
